1cfcc4464f4d3bdf5721d7e642b09efc5ef61ac3
phamngothanhdat123 <140497386+phamngothanhdat123@users.noreply.github.com>
2023-07-25 16:53:03 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+ var typed = new Typed('.element', {
+ strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker"],
+ typeSpeed:100,
+ backSpeed:100,
+ loop:true,
+ });
+$('.counter').countUp();
